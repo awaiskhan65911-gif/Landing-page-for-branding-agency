6cd2e86b99710582bb754396922301a47968a398
Awais khan <awaiskhan65911@gamil.com>
2025-08-21 19:58:19 +0500

    intro section p chnage

--- src/Components/IntroSection.jsx
+++ src/Components/IntroSection.jsx
@@ -3,8 +3,8 @@ import intro from "../assets/data/Introdata.json"
 
 export const IntroSection = () => {
     return (
-        <section className="relative  before:w-full before:left-0 before:-top-2 before:opacity-20  before:h-[1px] before:absolute before:bg-gradient-to-r before:from-[#4bb6ef] before:via-[#dd933e] before:to-[#d82e5a]">
-            <div className="relative max-w-[1456px] mx-auto px-8 ">
+        <section className="relative p-10 before:w-full before:left-0 before:-top-2 before:opacity-20  before:h-[1px] before:absolute before:bg-gradient-to-r before:from-[#4bb6ef] before:via-[#dd933e] before:to-[#d82e5a]">
+            <div className="relative max-w-[1456px] mx-auto ">
                 <div className="text-white  grid grid-cols-1 md:grid-cols-2 md:grid-rows-2  lg:grid-rows-1 lg:grid-cols-3  justify-between">
                     {intro.map((elem) => (
                         <div key={elem.id} className="px-2 group relative mt-4 text-center md:text-left">
